f60cbc2d244ee689e47c10f896b1b16c6b71a10b
AI-Python-QI <198759729@qq.com>
2018-06-08 19:02:45 +0800

    配置 版本迁移 工具 ，新建数据表 user  数据库为 mydb

---
+++ models/db.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import  sessionmaker
+
+
+
+
+
+HOST = '192.168.31.128'
+PORT = '3306'
+DATABASE = 'mydb'
+USERNAME = 'admin'
+PASSWORD = 'Root110qwe'
+
+
+DB_URL = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(
+    USERNAME,PASSWORD,HOST,PORT,DATABASE
+)
+
+engine = create_engine(DB_URL)
+DBSession = sessionmaker(bind=engine)
+Base = declarative_base(engine)
